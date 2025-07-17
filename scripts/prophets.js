0b190fecdc7266bd284f2eb0cd66c39fac0c1362
Boiketlo20 <nzimandeyolanda058@gmail.com>
2025-07-17 18:22:26 +0200

    Week 02: Latter Day Prophets

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -15,10 +15,15 @@ const displayProphets = (prophets) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let fullName = document.createElement('h2'); // fill in the blank
+        let dateOfBirth = document.createElement('p');
+        let placeOfBirth = document.createElement('p');
         let portrait = document.createElement('img');
+        
 
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place of Birth ${prophet.birthplace}`;
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.portrait} ______________`); // fill in the blank
@@ -28,6 +33,8 @@ const displayProphets = (prophets) => {
 
         // Append the section(card) with the created elements
         card.appendChild(fullName); //fill in the blank
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);
         card.appendChild(portrait);
 
         cards.appendChild(card);  
